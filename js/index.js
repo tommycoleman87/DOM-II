c43d9e8988591f44117a16d4497f41b44b736a4f
tommycoleman87 <tommiilee@gmail.com>
2019-06-18 17:35:38 -0400

    added some eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+var btn = document.querySelectorAll('.btn');
+var modal = document.querySelector(".modal");
+
+
+
+btn.forEach(btn => btn.addEventListener('mousedown', event => {
+    modal.style.display = 'block';
+   
+}));
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const pageImages = document.querySelectorAll('img');
+
+pageImages.forEach(image => image.addEventListener('mouseenter', event =>{
+    event.target.style.filter = 'invert(1)';
+    
+}));
+
+pageImages.forEach(image => image.addEventListener('mouseout', event =>{
+    event.target.style.filter = 'none';
+    
+}));
+
+
+
+
+
+
+
+
+
+
+
