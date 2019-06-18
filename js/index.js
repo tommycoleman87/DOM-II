56f9e84ca842ead5ec2f2021cb2667294e109c00
tommycoleman87 <tommiilee@gmail.com>
2019-06-18 18:10:47 -0400

    finished all 10 eventlisteners, stoppropagation, preventdefault

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 var btn = document.querySelectorAll('.btn');
 var modal = document.querySelector(".modal");
+var modalText = document.querySelector('.modal-content p')
 
 
 
@@ -35,6 +36,41 @@ pageImages.forEach(image => image.addEventListener('mouseout', event =>{
     event.target.style.filter = 'none';
     
 }));
+
+let navs = document.querySelectorAll('a');
+
+navs.forEach(a => a.addEventListener('dblclick', () => {
+    modal.style.display = "block";
+    modalText.textContent = 'Same Modal, different text';
+}));
+
+navs.forEach(a => a.addEventListener('click', () => {
+    event.preventDefault();
+}))
+
+var headerTxt = document.querySelector('.content-destination h2');
+var content = document.querySelector('.content-destination')
+
+headerTxt.addEventListener('mousedown', () => {
+    event.target.style.background = 'yellow';
+    event.stopPropagation();
+}) 
+
+content.addEventListener('mousedown', () => {
+    event.target.style.background = 'red';
+})
+
+
+const headerLogo = document.querySelector('.logo-heading');
+
+headerLogo.addEventListener('drag', () => {
+    event.target.textContent = 'Struggle Bus';
+})
+
+headerLogo.addEventListener('dragend', () => {
+    event.target.textContent = 'Fun Bus';
+})
+
 
 
 
